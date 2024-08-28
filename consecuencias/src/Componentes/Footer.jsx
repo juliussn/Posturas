83a2a90d056828525b497ace1cc5c52b7058b13b
DDHGDBNHDUEU <mbrodriguez@escuelasproa.edu.ar>
2024-08-28 11:27:03 -0300

    bakugou el heroe numero 1

--- consecuencias/src/Componentes/Footer.jsx
+++ consecuencias/src/Componentes/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
 
     return (
       <>
-        
+        <footer>hola</footer>
       </>
     )
   }
--- consecuencias/src/Componentes/Texto.jsx
+++ consecuencias/src/Componentes/Texto.jsx
@@ -2,7 +2,7 @@ function Texto() {
 
     return (
       <>
-        
+        <h3>emilia mernes te veo el viernes</h3>
       </>
     )
   }
